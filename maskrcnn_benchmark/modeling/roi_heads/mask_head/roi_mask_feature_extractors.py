82693d7283e2ea769e37a17c83d03085222d2dbe
Daniel Rose <droseger@protonmail.com>
2019-07-04 12:25:57 +0200

    Fix Mask R-CNN

--- maskrcnn_benchmark/modeling/roi_heads/mask_head/roi_mask_feature_extractors.py
+++ maskrcnn_benchmark/modeling/roi_heads/mask_head/roi_mask_feature_extractors.py
@@ -32,6 +32,7 @@ class MaskRCNNFPNFeatureExtractor(nn.Module):
         scales = cfg.MODEL.ROI_MASK_HEAD.POOLER_SCALES
         sampling_ratio = cfg.MODEL.ROI_MASK_HEAD.POOLER_SAMPLING_RATIO
         pooler = Pooler(
+            cfg,
             output_size=(resolution, resolution),
             scales=scales,
             sampling_ratio=sampling_ratio,
